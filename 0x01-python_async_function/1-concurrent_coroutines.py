7b19fa13d04b3903969a02c0a7871b20f92edbfd
Omo <omoikheloa56@gmail.com>
2024-08-05 14:51:05 +0000

    pep check

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" Executes multiple coroutines at the 
-    same time with async
-"""
+'''Executes multiple coroutines at the same time with async
+'''
 
 import asyncio
+import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -12,8 +12,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ List of the delays should be in ascending order.
     """
-    tasks = await asyncio.gather(
-        *list(map(lambda _: wait_random(max_delay), range(n)))
-    )
-
-    return sorted(tasks)
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
